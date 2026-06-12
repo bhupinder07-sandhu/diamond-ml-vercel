6e5078b2753ddad01684b5969777f91d9ecab2b4
bhupinder sandhu <bhupindersandhupcte@gmail.com>
2026-06-12 22:03:03 +0530

    Test both model

--- api/index.py
+++ api/index.py
@@ -4,7 +4,8 @@ import joblib
 app = Flask(__name__)
 
 price_model = joblib.load("models/price_model.pkl")
+cut_model = joblib.load("models/cut_model.pkl")
 
 @app.route("/")
 def home():
-    return "Price model loaded successfully!"
+    return "Both models loaded successfully!"
