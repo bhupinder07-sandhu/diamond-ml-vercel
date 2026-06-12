f03920e1eae1fc976b321bfc128a5daf9c8ebd0d
bhupinder sandhu <bhupindersandhupcte@gmail.com>
2026-06-12 21:25:25 +0530

    Load ML models

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import joblib
 
 app = Flask(__name__)
+
+price_model = joblib.load("models/price_model.pkl")
+cut_model = joblib.load("models/cut_model.pkl")
 
 @app.route("/")
 def home():
