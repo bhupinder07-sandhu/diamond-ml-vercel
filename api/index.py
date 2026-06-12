517307d09973fd05bce8c3309710987820da81af
bhupinder sandhu <bhupindersandhupcte@gmail.com>
2026-06-12 22:49:40 +0530

    Restore index.py

--- api/index.py
+++ api/index.py
@@ -1,21 +1,6 @@
 from flask import Flask, request, jsonify, render_template
-import joblib
 
 app = Flask(__name__, template_folder="../templates")
-
-# Load Models
-price_model = joblib.load("models/price_model.pkl")
-cut_model = joblib.load("models/cut_model.pkl")
-
-# Load Scalers
-price_scaler = joblib.load("models/price_scaler.pkl")
-cut_scaler = joblib.load("models/cut_scaler.pkl")
-
-# Load Encoders
-le_cut = joblib.load("models/le_cut.pkl")
-le_color = joblib.load("models/le_color.pkl")
-le_clarity = joblib.load("models/le_clarity.pkl")
-
 
 @app.route("/")
 def home():
