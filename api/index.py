7bc92f5194a013feeb4abc775aa366028a0fb0ae
bhupinder sandhu <bhupindersandhupcte@gmail.com>
2026-06-12 22:04:54 +0530

    Test all files

--- api/index.py
+++ api/index.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 price_model = joblib.load("models/price_model.pkl")
 cut_model = joblib.load("models/cut_model.pkl")
 
+price_scaler = joblib.load("models/price_scaler.pkl")
+cut_scaler = joblib.load("models/cut_scaler.pkl")
+
+le_cut = joblib.load("models/le_cut.pkl")
+le_color = joblib.load("models/le_color.pkl")
+le_clarity = joblib.load("models/le_clarity.pkl")
+
 @app.route("/")
 def home():
-    return "Both models loaded successfully!"
+    return "All files loaded successfully!"
